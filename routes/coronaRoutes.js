6d59c7b3103dc939414be056a864ea76fe7fdb29
rozakus <abdulrozak.mail@gmail.com>
2021-01-07 23:45:35 +0700

    add corona provinsi

--- routes/coronaRoutes.js
+++ routes/coronaRoutes.js
@@ -3,5 +3,6 @@ const CoronaController = require('../controllers/coronaController')
 
 router.get('/global', CoronaController.coronaGlobal)
 router.get('/indonesia', CoronaController.coronaIndonesia)
+router.get('/indonesia/provinsi', CoronaController.coronaIndonesiaProvinsi)
 
 module.exports = router
