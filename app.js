7d66c4d1bab84d3afe6f5269e974ace7f6dec4db
TommySus <tommysusanto77@gmail.com>
2021-01-07 15:59:19 +0700

    first-data

---
+++ app.js
@@ -0,0 +1,21 @@
+if(process.env.NODE_ENV !== "production") {
+   require('dotenv').config() 
+}
+
+const express = require('express')
+const app = express()
+const port = 3000
+const routes = require('./routes/index')
+const cors = require('cors')
+
+
+
+app.use(express.urlencoded({extended:true}))
+
+app.use(cors())
+app.use("/",routes)
+
+
+app.listen(port, ()=> {
+    console.log(`listening on port ${port}`)
+})
