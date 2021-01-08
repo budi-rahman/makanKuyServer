ddc52a359fc2902ccac6bff175f0670b2d115d37
rozakus <abdulrozak.mail@gmail.com>
2021-01-08 11:05:32 +0700

    add router search by cuisine and establishment from zomato

--- controllers/zomatoController.js
+++ controllers/zomatoController.js
@@ -7,8 +7,10 @@ class ZomatoController {
             let ZOMATO_KEY = '2cd294cc0248b7d31174330793f26d4a'
             let response = await Axios.get(URL, { headers: { 'user-key': ZOMATO_KEY } })
             // console.log(response.data)
+            let data = response.data.cuisines
+            data = data.map(detail => { return detail.cuisine })
 
-            return res.json(response.data)
+            return res.json({ data: data })
         } catch (err) {
             // console.log(err)
             return res.status(500).json(err)
@@ -22,7 +24,45 @@ class ZomatoController {
             let response = await Axios.get(URL, { headers: { 'user-key': ZOMATO_KEY } })
             // console.log(response.data)
 
-            return res.json(response.data)
+            let data = response.data.establishments
+            data = data.map(detail => {
+                return {
+                    establishment_id: detail.establishment.id,
+                    establishment_name: detail.establishment.name,
+                }
+            })
+
+            return res.json({ data: data })
+        } catch (err) {
+            // console.log(err)
+            return res.status(500).json(err)
+        }
+    }
+
+    static async all(req, res) {
+        try {
+            console.log('>>> params : ', req.params)
+
+            let URL = `https://developers.zomato.com/api/v2.1/search?entity_id=74&entity_type=city`
+            let ZOMATO_KEY = '980fda904333cc2eb792a7775c6f12bc'
+            let response = await Axios.get(URL, { headers: { 'user-key': ZOMATO_KEY } })
+            let temp = response.data.restaurants
+            let result = []
+            for (let i = 0; i < temp.length; i++) {
+                // console.log(temp[i])
+
+                result.push({
+                    name: temp[i].restaurant.name,
+                    address: temp[i].restaurant.location.address,
+                    cuisines: temp[i].restaurant.cuisines,
+                    average_cost_for_two: temp[i].restaurant.average_cost_for_two,
+                    user_rating: temp[i].restaurant.user_rating.aggregate_rating,
+                    featured_image: temp[i].restaurant.featured_image,
+                    timings: temp[i].restaurant.timings
+                })
+            }
+
+            return res.json({ data: result })
         } catch (err) {
             // console.log(err)
             return res.status(500).json(err)
@@ -31,24 +71,95 @@ class ZomatoController {
 
     static async search(req, res) {
         try {
-            let URL = `https://developers.zomato.com/api/v2.1/search?entity_id=74&count=20`
+            console.log('>>> params : ', req.params)
+            let { cuisine_id, establishment_id } = req.params
+            cuisine_id ? cuisine_id : 1040
+            establishment_id ? establishment_id : 1
+
+            let URL = `https://developers.zomato.com/api/v2.1/search?entity_id=74&entity_type=city&cuisines=${cuisine_id}&establishment_type=${establishment_id}`
             let ZOMATO_KEY = '980fda904333cc2eb792a7775c6f12bc'
             let response = await Axios.get(URL, { headers: { 'user-key': ZOMATO_KEY } })
             let temp = response.data.restaurants
             let result = []
             for (let i = 0; i < temp.length; i++) {
-               result.push({
-                   name : temp[i].restaurant.name,
-                   address : temp[i].restaurant.location.address,
-                   cuisines : temp[i].restaurant.cuisines,
-                   average_cost_for_two : temp[i].restaurant.average_cost_for_two,
-                   user_rating : temp[i].restaurant.user_rating.aggregate_rating,
-                   featured_image : temp[i].restaurant.featured_image,
-                   timings : temp[i].restaurant.timings
-               })
+                // console.log(temp[i])
+
+                result.push({
+                    name: temp[i].restaurant.name,
+                    address: temp[i].restaurant.location.address,
+                    cuisines: temp[i].restaurant.cuisines,
+                    average_cost_for_two: temp[i].restaurant.average_cost_for_two,
+                    user_rating: temp[i].restaurant.user_rating.aggregate_rating,
+                    featured_image: temp[i].restaurant.featured_image,
+                    timings: temp[i].restaurant.timings
+                })
             }
 
-            return res.json(result)
+            return res.json({ data: result })
+        } catch (err) {
+            // console.log(err)
+            return res.status(500).json(err)
+        }
+    }
+
+    static async searchCuisine(req, res) {
+        try {
+            console.log('>>> params : ', req.params)
+            let { cuisine_id } = req.params
+            cuisine_id ? cuisine_id : 1040
+
+            let URL = `https://developers.zomato.com/api/v2.1/search?entity_id=74&entity_type=city&cuisines=${cuisine_id}`
+            let ZOMATO_KEY = '980fda904333cc2eb792a7775c6f12bc'
+            let response = await Axios.get(URL, { headers: { 'user-key': ZOMATO_KEY } })
+            let temp = response.data.restaurants
+            let result = []
+            for (let i = 0; i < temp.length; i++) {
+                // console.log(temp[i])
+
+                result.push({
+                    name: temp[i].restaurant.name,
+                    address: temp[i].restaurant.location.address,
+                    cuisines: temp[i].restaurant.cuisines,
+                    average_cost_for_two: temp[i].restaurant.average_cost_for_two,
+                    user_rating: temp[i].restaurant.user_rating.aggregate_rating,
+                    featured_image: temp[i].restaurant.featured_image,
+                    timings: temp[i].restaurant.timings
+                })
+            }
+
+            return res.json({ data: result })
+        } catch (err) {
+            // console.log(err)
+            return res.status(500).json(err)
+        }
+    }
+
+    static async searchEstablishment(req, res) {
+        try {
+            console.log('>>> params : ', req.params)
+            let { establishment_id } = req.params
+            establishment_id ? establishment_id : 1
+
+            let URL = `https://developers.zomato.com/api/v2.1/search?entity_id=74&entity_type=city&establishment_type=${establishment_id}`
+            let ZOMATO_KEY = '980fda904333cc2eb792a7775c6f12bc'
+            let response = await Axios.get(URL, { headers: { 'user-key': ZOMATO_KEY } })
+            let temp = response.data.restaurants
+            let result = []
+            for (let i = 0; i < temp.length; i++) {
+                // console.log(temp[i])
+
+                result.push({
+                    name: temp[i].restaurant.name,
+                    address: temp[i].restaurant.location.address,
+                    cuisines: temp[i].restaurant.cuisines,
+                    average_cost_for_two: temp[i].restaurant.average_cost_for_two,
+                    user_rating: temp[i].restaurant.user_rating.aggregate_rating,
+                    featured_image: temp[i].restaurant.featured_image,
+                    timings: temp[i].restaurant.timings
+                })
+            }
+
+            return res.json({ data: result })
         } catch (err) {
             // console.log(err)
             return res.status(500).json(err)
--- routes/zomatoRoutes.js
+++ routes/zomatoRoutes.js
@@ -3,6 +3,9 @@ const zomatoController = require('../controllers/zomatoController')
 
 router.get('/cuisines', zomatoController.cuisines)
 router.get('/establishments', zomatoController.establishments)
-router.get('/search', zomatoController.search)
+router.get('/all', zomatoController.all)
+router.get('/search/cuisine/:cuisine_id', zomatoController.searchCuisine)
+router.get('/search/establishment/:establishment_id', zomatoController.searchEstablishment)
+router.get('/search/:cuisine_id/:establishment_id', zomatoController.search)
 
 module.exports = router
