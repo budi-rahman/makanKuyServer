d34306a0a8201b4dee222fc32a67d39a3da8563f
rozakus <abdulrozak.mail@gmail.com>
2021-01-08 14:49:42 +0700

    rozakus add google login / register

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const {UserController} = require('../controllers/userController')
-const {register} = require('../controllers/userController')
+const { UserController } = require('../controllers/userController')
+const { register } = require('../controllers/userController')
 
 router.post('/register', register)
 router.post('/login', UserController.login)
+router.post('/loginGoogle', UserController.loginGoogle)
 
 module.exports = router
